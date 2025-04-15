8020e9ea8f22c733c4cdc7a42abc38627c73f2c1
Odu-Enkay <[erozonankiru@gmail.com]>
2025-04-15 02:31:09 -0600

    Added the url:id

--- express_server.js
+++ express_server.js
@@ -1,6 +1,8 @@
 const express = require("express");
 const app = express();
-const PORT = 8080; // default port 8080
+const PORT = 8080;
+
+app.set("view engine", "ejs");
 
 const urlDatabase = {
   b2xVn2: "http://www.lighthouselabs.ca",
@@ -13,6 +15,17 @@ app.get("/", (req, res) => {
 
 app.get("/urls.json", (req, res) => {
   res.json(urlDatabase);
+});
+
+app.get("/urls", (req, res) => {
+  const templateVars = {urls: urlDatabase};
+  res.render("urls_index",templateVars);
+});
+
+app.get("/urls/:id", (req, res) => {
+  const longURL = urlDatabase[req.params.id];
+  const templateVars = { id: req.params.id, longURL: longURL };
+  res.render("urls_show", templateVars);
 });
 
 app.get("/hello", (req, res) => {
