6ea0ef1caa80510bc4a14161ed4265da0133faa6
Odu-Enkay <[erozonankiru@gmail.com]>
2025-04-15 15:20:20 -0600

    implemented adding short/longURL to the DB

--- express_server.js
+++ express_server.js
@@ -1,31 +1,40 @@
 const express = require("express");
 const app = express();
 const PORT = 8080;
+const crypto = require('crypto');
 
-app.use(express.urlencoded({ extended: true }));
-function generateRandomString() {
-  
-}
 
 app.set("view engine", "ejs");
 
 const urlDatabase = {
   b2xVn2: "http://www.lighthouselabs.ca",
-  "9sm5xK": "http://www.google.com",
+  "9sm5xK": "http://www.google.com"
 };
 
-app.get("/", (req, res) => {
-  res.send("Hello!");
+// Function to generate a random string
+function generateRandomString() {
+  return crypto.randomBytes(6).toString('hex');
+}
+
+app.use(express.urlencoded({ extended: true })); // this lets you take data from a form
+
+pp.get("/urls", (req, res) => {
+  const templateVars = { urls: urlDatabase }; // check with 
+  res.render("urls_index", templateVars);
 });
 
-app.get("/urls.json", (req, res) => {
-  res.json(urlDatabase);
+app.get('/u/:id', (req, res) => {
+  const shortURLID = req.params.id;    // fetches the user input and store inthe shortURLID
+  const longURL = urlDatabase[shortURLID];
+
+  if (longURL) {
+    res.redirect(longURL);
+  } else {
+    res.status(404).send('URL not found.'); // Handle non-existent short URLs
+  }
 });
 
-app.get("/urls", (req, res) => {
-  const templateVars = {urls: urlDatabase};
-  res.render("urls_index",templateVars);
-});
+// More routes and server setup...
 
 app.get("/urls/new", (req, res) => {
   res.render("urls_new");
@@ -37,15 +46,29 @@ app.get("/urls/:id", (req, res) => {
   res.render("urls_show", templateVars);
 });
 
-app.get("/hello", (req, res) => {
-  res.send("<html><body>Hello <b>World</b></body></html>\n");
+/*app.get("/", (req, res) => {
+  res.send("Hello!");
 });
 
-app.post("/urls", (req, res) => {
-  console.log(req.body); // Log the POST request body to the console
-  res.send("Ok"); // Respond with 'Ok' (we will replace this)
+app.get("/urls.json", (req, res) => {
+  res.json(urlDatabase);
 });
+
+app.get("/hello", (req, res) => {
+  res.send("<html><body>Hello <b>World</b></body></html>\n");
+}); */
 
 app.listen(PORT, () => {
   console.log(`Example app listening on port ${PORT}!`);
+});
+
+// Post Methods Here
+app.post('/urls', (req, res) => {
+  const longURL = req.body.longURL;
+  const shortURLID = generateRandomString();
+  
+  urlDatabase[shortURLID] = longURL;
+  
+  // Redirect to the page showing the new short URL
+  res.redirect(`/urls/${shortURLID}`);
 });
